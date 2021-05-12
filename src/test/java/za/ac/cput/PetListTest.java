b8d8736712b2ec88ecb68b722539b8a97e441077
TheMorningStar <216061324@mycput.ac.za>
2021-05-12 16:23:27 +0200

    Added Remove Test

--- src/test/java/za/ac/cput/PetListTest.java
+++ src/test/java/za/ac/cput/PetListTest.java
@@ -14,14 +14,27 @@ public class PetListTest {
 
     List<Pet> petList = new ArrayList<>();
 
-    @Test
-    //List Add
-    void test(){
+    public void fillList(){
         for (int i = 0; i < 10; i++){
-            String name = "n" + 1;
+            String name = "Pet" + i;
             Pet p = new Pet(name);
             petList.add(p);
         }
-        assertEquals(10, petList.size());
+    }
+
+    @Test
+    //List Add
+    void testAdd(){
+        fillList();
+        petList.add(10, new Pet("Pet10"));
+        assertEquals(11, petList.size());
+    }
+
+    @Test
+        //List remove
+    void testRemove(){
+        fillList();
+        petList.remove(7);
+        assertEquals(9,petList.size());
     }
 }
