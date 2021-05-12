8bb69f27d29aaeba7f5bb69321f26450a2a0a46f
TheMorningStar <216061324@mycput.ac.za>
2021-05-12 15:17:03 +0200

    updated ignore

--- src/main/java/za/ac/cput/Pet.java
+++ src/main/java/za/ac/cput/Pet.java
@@ -6,4 +6,34 @@ package za.ac.cput;
 
 public class Pet {
 
+    private String name;
+    private int age;
+
+    public Pet(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
